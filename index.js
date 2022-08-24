049e11f1bdd07c59bd1a29af2ef336dd1c172c34
Vishal Kr Singh <vishalkrsingh1612@gmail.com>
2022-08-24 23:53:12 +0530

    added view engine and routes

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const port = 8003;
 
 //use express router
 app.use('/',require('./routes'));
+// setup the view engine
+app.set('view engine','ejs');
+app.set('views',('./views'));
 
 app.listen(port,function(err){
     if(err){
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const homeController = require('../controllers/home_controller');
+console.log("router loaded");
 router.get('/',homeController.home);
+
+router.use('/user',require('./user'));
+
 console.log('router loaded');
 module.exports = router; 
